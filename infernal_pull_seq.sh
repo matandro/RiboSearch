78691bab9a4586069b96d77c41bdf47103f3dfd4
cs <cs@barashsrv.cs.bgu.ac.il>
2018-01-24 14:40:06 +0200

    infernal now returns sequence and target name only, used solution from Eric Nawrocki

---
+++ infernal_pull_seq.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Usage: sh infernal_pull_seq.sh <tlbout file> <input fasta file> <output fasta>
+
+TLBOUT_FILE = $1
+INPUT_FASTA = $2
+OUTPUT_FASTA = $3
+
+ESL_SFETCH_LOC = "/opt/algorithm/infernal/bin/esl-sfetch"
+
+if [ -s $TLBOUT_FILE ] 
+then
+    cat $TLBOUT_FILE | grep -v ^\# | grep ! | awk '{ printf("%s/%d-%d %d %d %s\n", $1, $8, $9, $8, $9, $1); }' | $ESL_SFETCH_LOC -Cf $INPUT_FASTA - > $OUTPUT_FASTA 
+fi
+
